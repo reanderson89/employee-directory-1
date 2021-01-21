85e04a365ff594a8ae1a8b16b92a1612fa23a427
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-01-21 17:04:05 -0500

    Basic employee row layout hardcoded

---
+++ src/components/Employee/EmployeeRow.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const EmployeeRow = () => {
+  return (
+    <>
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          <th scope="col">Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col">Email</th>
+          <th scope="col">DOB</th>
+        </tr>
+      </thead>
+      <tbody>
+          <td>img here</td>
+          <td>name here</td>
+          <td>phone number here</td>
+          <td>email here</td>
+          <td> DOB here</td>
+      </tbody>
+    </>
+  );
+};
+
+export default EmployeeRow;
