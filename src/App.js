62a0a79467a037b5807dc4797e9361eae6498d8d
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-01-21 16:15:52 -0500

    Added bootstrap and created initial folders for employees

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import { HashRouter as Router, Route} from "react-router-dom";
+
 function App() {
   return (
    <Router basename="/">
-     
+     {/* <h1> hello world</h1> */}
    </Router>
   );
 }
