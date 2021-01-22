6ebecaba4270e71c147fff3e0529ff8380a7ce2b
Stephany Bolivar <g.stephanybolivar@gmail.com>
2021-01-22 16:54:05 -0500

    Set sorting on name column

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import EmployeeRow from "../components/Employee/EmployeeRow";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 const Home = () => {
   // setting state
   const [employees, setEmployees] = useState([]);
+  const [sortOrder, setSortOrder] = useState('');
 
   //   Initial employee render
   useEffect(() => {
@@ -20,6 +22,26 @@ const Home = () => {
         console.log(err);
       });
   }, []);
+
+  // sort name in table
+  const sortBy = () => {
+    let sortedEmployees = [];
+
+    if (!sortOrder || sortOrder === "ascending") {
+      sortedEmployees = [...employees].sort((a, b) =>
+        a.name.first < b.name.first ? -1 : 1
+      );
+      setSortOrder("descending");
+    } else {
+      sortedEmployees = [...employees].sort((a, b) =>
+        a.name.first > b.name.first ? -1 : 1
+      );
+      setSortOrder("ascending");
+    }
+    console.log(sortedEmployees);
+    setEmployees(sortedEmployees);
+  };
+
   //   return a table with employees information
   return (
     <div className="container">
@@ -29,19 +51,28 @@ const Home = () => {
         </div>
       </div>
       <div className="row">
-        <table className="table table-striped">
+        <div className="col-sm-3">
+          <SearchBar />
+        </div>
+      </div>
+      <div className="row">
+        <table className="table sortable table-striped">
           <thead>
             <tr>
               <th scope="col">Image</th>
-              <th scope="col">Name</th>
+              <th scope="col" onClick={sortBy}>Name</th>
               <th scope="col">Phone</th>
               <th scope="col">Email</th>
-              
             </tr>
           </thead>
           <tbody>
             {employees.map((employee) => (
-              <EmployeeRow name={employee.name} phone={employee.phone} email={employee.email} picture={employee.picture} />
+              <EmployeeRow
+                name={employee.name}
+                phone={employee.phone}
+                email={employee.email}
+                picture={employee.picture}
+              />
             ))}
           </tbody>
         </table>
